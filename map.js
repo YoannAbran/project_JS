7aaafaef6474b4f5fe738fe0ae6fe6202dcc4b68
Yoann Abran <y.abran@codeur.online>
2020-06-18 17:02:44 +0200

    block aléatoire

--- map.js
+++ map.js
@@ -1,32 +1,65 @@
-const H_GRID = 20;
-const V_GRID = 20;
-const GRID_SIZE = 40;
-const WINDOW_WIDTH = H_GRID * GRID_SIZE;
-const WINDOW_HEIGHT = V_GRID * GRID_SIZE;
-
-var plateau = document.getElementById('plateau');
-plateau.style.width = WINDOW_WIDTH;
-plateau.style.height = WINDOW_HEIGHT;
-
-
-
-
-var blockGrid = [];
-for(var i=0 ; i < H_GRID; i++){
-  blockGrid.push([]);
-  for(var j=0 ; j < V_GRID ; j++){
-    let block=document.createElement('div');
-    block.style.width='40px';
-    block.style.height='40px';
-    block.style.display='flex';
-    block.style.position='absolute';
-    block.style.backgroundColor='black';
-    block.style.marginLeft = (i * GRID_SIZE).toString()+'px';
-    block.style.marginTop = (j * GRID_SIZE).toString()+'px';
-
-    document.getElementById("plateau").appendChild(block);
-    blockGrid[i].push(block);
-  }
-}
-
-blockGrid[10][10].style.backgroundColor = 'red';
+// const H_GRID = 20;
+// const V_GRID = 20;
+// const GRID_SIZE = 40;
+// const WINDOW_WIDTH = H_GRID * GRID_SIZE;
+// const WINDOW_HEIGHT = V_GRID * GRID_SIZE;
+//
+// var plateau = document.getElementById('plateau');
+// plateau.style.width = WINDOW_WIDTH;
+// plateau.style.height = WINDOW_HEIGHT;
+//
+//
+//
+//
+// var blockGrid = [];
+// for(var i=0 ; i < H_GRID; i++){
+//   blockGrid.push([]);
+//   for(var j=0 ; j < V_GRID ; j++){
+//     let block=document.createElement('div');
+//     block.style.width='40px';
+//     block.style.height='40px';
+//     block.style.display='flex';
+//     block.style.position='absolute';
+//
+//     if (random100()>70){ block.style.backgroundColor='black';
+//     block.traverser = false;
+//   }
+//     else {
+//     block.style.backgroundColor='white';
+//     block.traverser = true;
+//   }
+//     block.style.marginLeft = (i * GRID_SIZE).toString()+'px';
+//     block.style.marginTop = (j * GRID_SIZE).toString()+'px';
+//
+//     document.getElementById("plateau").appendChild(block);
+//     blockGrid[i].push(block);
+//   }
+// }
+//
+// function randomColor(){
+// return "#" + ((1<<24)*Math.random()|0).toString(16);
+// }
+// function random100() {
+// return Math.floor(Math.random() * 100);
+// }
+// var plateau = document.getElementById("centreplat");
+// var uneBrique;
+//
+// function fabriqueBriques() {
+//
+// //90 brique pour un espace de 40px entre;
+//
+//   for (var i = 0; i < 81; i++) {
+//     // fabrique une brique
+//     uneBrique = document.createElement("div")
+//
+//     // Ajoute une classe à cette brique pour lui donner un style via CSS
+//     uneBrique.className = "classBrique";
+//
+//     // Insére cette brique dans la div ZoneDeJeu
+//     plateau.appendChild(uneBrique);
+//   }
+//
+// }
+// //  Lance la fonction
+// fabriqueBriques();
--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 
+const H_GRID = 20;
+const V_GRID = 20;
+const GRID_SIZE = 40;
+const WINDOW_WIDTH = H_GRID * GRID_SIZE;
+const WINDOW_HEIGHT = V_GRID * GRID_SIZE;
+
+
 var pion = document.getElementById('pion'),
     s = pion.style, // Un petit raccourci
     x = pion.offsetLeft, // On récupère la position absolue initiale.
     y = pion.offsetTop;
+
+
+
+var plateau = document.getElementById('plateau');
+plateau.style.width = WINDOW_WIDTH;
+plateau.style.height = WINDOW_HEIGHT;
+
+
+
+
+var blockGrid = [];
+for(var i=0 ; i < H_GRID; i++){
+  blockGrid.push([]);
+  for(var j=0 ; j < V_GRID ; j++){
+    let block=document.createElement('div');
+    block.style.width='40px';
+    block.style.height='40px';
+    block.style.display='flex';
+    block.style.position='absolute';
+
+    if (random100()>70){ block.style.backgroundColor='black';
+    block.traverser = false;
+  }
+    else {
+    block.style.backgroundColor='white';
+    block.traverser = true;
+  }
+    block.style.marginLeft = (i * GRID_SIZE).toString()+'px';
+    block.style.marginTop = (j * GRID_SIZE).toString()+'px';
+
+    document.getElementById("plateau").appendChild(block);
+    blockGrid[i].push(block);
+  }
+}
 document.onkeydown = function(event){
     var event = event || window.event,
         keyCode = event.keyCode;
@@ -10,47 +51,31 @@ document.onkeydown = function(event){
     // On détecte l'événement puis selon la fleche, on incrémente ou décrément les variables globales de position, x et y.
     switch(keyCode){
     case 38:
-        y-=10;
+        y-=GRID_SIZE;
         break;
     case 40:
-        y+=10;
+        y+=GRID_SIZE;
         break;
     case 37:
-        x-=10;
+        x-=GRID_SIZE;
         break;
     case 39:
-        x+=10;
+        x+=GRID_SIZE;
         break;
     }
     // Et enfin on applique les modifications :
 
 		if (x < 0) x = 0;
 		if (y < 0) y = 0;
-		if (x > 760 ) x=760;
+		if (x > WINDOW_WIDTH-GRID_SIZE && blockGrid[y/GRID_SIZE][x/GRID_SIZE].traverser ) x=760;
 		if (y > 760 ) y=760;
 
 		s.left = String(x)+'px';
 		s.top = String(y)+'px';
 }
-
-// var plateau = document.getElementById("centreplat");
-// var uneBrique;
-//
-// function fabriqueBriques() {
-//
-// //90 brique pour un espace de 40px entre;
-//
-//   for (var i = 0; i < 81; i++) {
-//     // fabrique une brique
-//     uneBrique = document.createElement("div")
-//
-//     // Ajoute une classe à cette brique pour lui donner un style via CSS
-//     uneBrique.className = "classBrique";
-//
-//     // Insére cette brique dans la div ZoneDeJeu
-//     plateau.appendChild(uneBrique);
-//   }
-//
-// }
-// //  Lance la fonction
-// fabriqueBriques();
+function randomColor(){
+return "#" + ((1<<24)*Math.random()|0).toString(16);
+}
+function random100() {
+return Math.floor(Math.random() * 100);
+}
